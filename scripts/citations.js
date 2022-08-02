d70135b0211ae65b2f55226e606f16451ea2f2b1
SinGala67 <galahane.rouille@gmail.com>
2022-08-02 06:16:54 +0200

    Base structure index

--- scripts/citations.js
+++ scripts/citations.js
@@ -1,16 +1,17 @@
 var citationText = document.getElementById("citation_text");
 
-var citationsArray = [
-    "'Le vrai bonheur ne dépend d'aucun être, d'aucun object extérieur. Il ne dépend que de nous...' - Dalaï Lama",
-    "'Que chaque événement de votre vie vous apprenne quelque chose.' - Osho",
-    "'Vous ne pouvez être que ce que vous êtes. Détendez-vous ! L'existence a besoin de vous tel que vous êtes.' - Osho",
-    "'Le bonheur est comme un don qu’on doit cultiver. Tant qu’on ne le cultive pas, on n’obtient rien.' - O. M. Aïvanhov",
-    "'Nul besoin de temples, nul besoin de philosophies compliquées. Notre cerveau et notre coeur sont nos temples.' - Dalaï Lama",
-    "'L'apaisement réside en chacun de nous.' - Dalaï Lama",
-    "'L’amour n’est pas une relation, c’est une nouvelle façon d’être. Je ne me contente plus d’aimer ceci ou cela… Je deviens amour et cet amour déborde de mon coeur conscient.' - Osho",
-    "'En prononçant le mot merci, c’est comme une source de lumière, de paix et de joie que vous faites jaillir dans votre âme..' - O. M. Aïvanhov",
-    "'Cherchez la sagesse, l’amour, la vérité, la justice, la bonté, car alors, où que vous soyez, quelles que soient les conditions, vous vous sentirez libre..' - O. M. Aïvanhov",
-    "'Pour apporter le bonheur aux autres, il faut surtout remplir son cœur d’amour..' - O. M. Aïvanhov"
+var citationsArray = 
+[
+    "'Le vrai bonheur ne dÃ©pend d'aucun Ãªtre, d'aucun object extÃ©rieur. Il ne dÃ©pend que de nous...' - DalaÃ¯ Lama",
+    "'Que chaque Ã©vÃ©nement de votre vie vous apprenne quelque chose.' - Osho",
+    "'Vous ne pouvez Ãªtre que ce que vous Ãªtes. DÃ©tendez-vous ! L'existence a besoin de vous tel que vous Ãªtes.' - Osho",
+    "'Le bonheur est comme un don quâ€™on doit cultiver. Tant quâ€™on ne le cultive pas, on nâ€™obtient rien.' - O. M. AÃ¯vanhov",
+    "'Nul besoin de temples, nul besoin de philosophies compliquÃ©es. Notre cerveau et notre coeur sont nos temples.' - DalaÃ¯ Lama",
+    "'L'apaisement rÃ©side en chacun de nous.' - DalaÃ¯ Lama",
+    "'Lâ€™amour nâ€™est pas une relation, câ€™est une nouvelle faÃ§on dâ€™Ãªtre. Je ne me contente plus dâ€™aimer ceci ou celaâ€¦ Je deviens amour et cet amour dÃ©borde de mon coeur conscient.' - Osho",
+    "'En prononÃ§ant le mot merci, câ€™est comme une source de lumiÃ¨re, de paix et de joie que vous faites jaillir dans votre Ã¢me..' - O. M. AÃ¯vanhov",
+    "'Cherchez la sagesse, lâ€™amour, la vÃ©ritÃ©, la justice, la bontÃ©, car alors, oÃ¹ que vous soyez, quelles que soient les conditions, vous vous sentirez libre..' - O. M. AÃ¯vanhov",
+    "'Pour apporter le bonheur aux autres, il faut surtout remplir son cÅ“ur dâ€™amour..' - O. M. AÃ¯vanhov"
 ];
 
 var randNumber;
